b0639f754adc2a7b2d01a3674834f4a09c1adcf6
Rudy Ravelin <rudy@noukod.com>
2019-01-29 19:45:26 +0000

    Automatically backed up by Learn

--- spec/features/artists_spec.rb
+++ spec/features/artists_spec.rb
@@ -9,26 +9,26 @@ describe "artists", type:  :feature do
     @voyager = @artist.songs.create!(title: "Voyager")
   end
 
-  # it "links to the artist's songs by title" do
-  #   visit artist_path(@artist)
-  #   expect(page).to have_link("The Grid", href: song_path(@grid))
-  # end
-  # 
-  # it "lists all of the artist's songs" do
-  #   visit artist_path(@artist)
-  #   within("ul") do
-  #     expect(page).to have_content("The Grid")
-  #     expect(page).to have_content("Voyager")
-  #   end
-  # end
-  # 
-  # it "lists the artists" do
-  #   visit artists_path
-  #   expect(page).to have_content("Daft Punk")
-  # end
-  # 
-  # it "shows the song count for each artist" do
-  #   visit artists_path
-  #   expect(page).to have_content("2 songs")
-  # end
+  it "links to the artist's songs by title" do
+    visit artist_path(@artist)
+    expect(page).to have_link("The Grid", href: song_path(@grid))
+  end
+
+  it "lists all of the artist's songs" do
+    visit artist_path(@artist)
+    within("ul") do
+      expect(page).to have_content("The Grid")
+      expect(page).to have_content("Voyager")
+    end
+  end
+
+  it "lists the artists" do
+    visit artists_path
+    expect(page).to have_content("Daft Punk")
+  end
+
+  it "shows the song count for each artist" do
+    visit artists_path
+    expect(page).to have_content("2 songs")
+  end
 end
